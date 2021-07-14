35264f37a4f4ceeec5c9b82c5fd39e2681d6b2bf
Tur1st4 <paulo.eduardo.m.jacob@gmail.com>
2021-07-14 17:12:28 -0300

    Terminando os tipos básicos

--- 04_tipos_basicos.dart
+++ 04_tipos_basicos.dart
@@ -16,4 +16,28 @@ main() {
   dynamic x = 'Top d+';
   x = 123;
   print(x);
+
+  List aprovados = ['Paulo', 'Marcos'];
+  print(aprovados[0]);
+  print(aprovados.elementAt(1));
+  print(aprovados.length);
+
+  Map telefones = {
+    'Paulo': '14 99888-0000',
+    'Marcos': '11 99878-8888'
+  };
+  print(telefones['Paulo']);
+  print(telefones.length);
+  print(telefones.values);
+  print(telefones.keys);
+  print(telefones.entries);
+
+  Set times = {
+    'Brasil',
+    'Alemanha'
+  };
+  times.add('Japão');
+  print(times.first);
+  print(times.last);
+  print(times.contains('Brasil'));
 }
