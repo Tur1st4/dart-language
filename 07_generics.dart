f035d17249aeff59512de6f2b4ade8ae1942449c
Tur1st4 <paulo.eduardo.m.jacob@gmail.com>
2021-07-15 20:59:27 -0300

    Aprendendo generics

---
+++ 07_generics.dart
@@ -0,0 +1,9 @@
+main() {
+  var listaCoisas = ['Banana', true, 123, 4.56];
+  List<String> frutas = ['Banana', 'Maça'];
+
+  Map<String, double> salarios = {
+    'Joao': 1234.44,
+    'Paulo': 16345.33
+  };
+}
