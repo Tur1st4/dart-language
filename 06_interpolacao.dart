96a1ddf6fa5b103cb457494603fc0d75be1eca43
Tur1st4 <paulo.eduardo.m.jacob@gmail.com>
2021-07-15 20:48:00 -0300

    Estudando interpolacao

---
+++ 06_interpolacao.dart
@@ -0,0 +1,9 @@
+main() {
+  String nome = 'Paulo';
+  String status = 'Aprovado';
+  double nota = 9.2;
+
+  print(nome + ' ' + status + ' ' + nota.toString());
+  print('$nome $status $nota');
+  print('1 + 1 = ${1 + 1}');
+}
