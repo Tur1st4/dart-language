7d67fbb8b13c9c9e9219b446ac1c3b2d872c8161
Tur1st4 <paulo.eduardo.m.jacob@gmail.com>
2021-07-18 19:34:31 -0300

    Aprendendo for

--- 09-for.dart
+++ 09-for.dart
@@ -2,4 +2,17 @@ main() {
   for (int a = 0; a <= 10; a++) {
     print(a);
   }
+
+  for (int a = 100; a > 0; a -= 4) {
+    print(a);
+  }
+
+  List notas = [8.2, 9.3, 10.0];
+  for (int i = 0; i < notas.length; i++) {
+    print(notas[i]);
+  }
+
+  for (double nota in notas) {
+    print(nota);
+  }
 }
