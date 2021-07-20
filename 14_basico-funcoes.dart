2b678b3ec5ff27f75685c0dfc06dc29e7605ebb0
Paulo <paulo.eduardo.m.jacob@gmail.com>
2021-07-20 20:01:21 -0300

    Terminando o básico de funções

--- 14_basico-funcoes.dart
+++ 14_basico-funcoes.dart
@@ -9,8 +9,14 @@ void somaAleatorio() {
   print(Random().nextInt(10) + Random().nextInt(10));
 }
 
+int soma(int valor_a, int valor_b) {
+  return valor_a + valor_b;
+}
+
 void main() {
   somaComPrint(2, 3);
   somaComPrint(5, 7);
   somaAleatorio();
+  int numero = soma(2, 11);
+  print("Soma: $numero");
 }
