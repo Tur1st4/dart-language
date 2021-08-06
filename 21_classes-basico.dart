17579a563e0be344cd83dbc908f16a46a4abda07
Tur1st4 <paulo.eduardo.m.jacob@gmail.com>
2021-08-05 21:26:45 -0300

    Construtores nomeados

--- 21_classes-basico.dart
+++ 21_classes-basico.dart
@@ -4,6 +4,11 @@ class Data {
   late int ano;
 
   Data(this.dia, this.mes, this.ano);
+  Data.DiaAleatorio({this.dia = 1, this.mes = 12, this.ano = 1987});
+  Data.outroDia(this.ano) {
+    dia = 30;
+    mes = 12;
+  }
 
   String formatar() {
     return '${dia}/${mes}/${ano}';
@@ -16,7 +21,10 @@ class Data {
 
 main() {
   Data aniversario = new Data(26, 6, 2021);
+  Data data = Data.DiaAleatorio(ano: 2018, mes: 12, dia: 24);
 
   print(aniversario.formatar());
-  print(aniversario); 
+  print(aniversario);
+  print(data);
+  print(Data.outroDia(2013));
 }
